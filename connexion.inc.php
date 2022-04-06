4b124e972b4ef45345d23d0f1e6b5f23f06f5598
Kilouhardi <Joshual.courriel@gmail.com>
2022-04-06 10:56:43 +0200

    page profil + connec

---
+++ connexion.inc.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * création d'objet PDO de la connexion qui sera représenté par la variable $cnx
+ */
+$user =  "joshua.lemoine";
+$pass =  "MDPpostgre2003";
+$host = "pgsql:host=sqletud.u-pem.fr;dbname='joshua.lemoine_db'";
+
+try {
+    $cnx = new PDO($host,$user,$pass);  
+}
+catch (PDOException $e) {
+    echo "ERREUR : La connexion a échouée";
+
+ /* Utiliser l'instruction suivante pour afficher le détail de erreur sur la
+ * page html. Attention c'est utile pour débugger mais cela affiche des
+ * informations potentiellement confidentielles donc éviter de le faire pour un
+ * site en production.*/
+//    echo "Error: " . $e;
+
+}
+
+?>
+
