1fcaa33b7b5273794af4093eb00f73e58dc55829
Meledit <99364115+Meledit@users.noreply.github.com>
2022-05-10 23:07:49 +0200

    css

--- ajoutAnimal.php
+++ ajoutAnimal.php
@@ -9,95 +9,100 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styleAjout.css">
-    <title>Ajouter un animal</title>
+    <link rel="stylesheet" href="animal.css">
+    <link rel="stylesheet" href="styleProfil.css">
+    <title>Un nouvel ami ...</title>
 </head>
 <body>
-    
     <?php
         include 'connexion.inc.php';      
     ?>
+    <div class="border">
+        <a href='index.php' class='btn'><img src='photo/accueil 1.png' alt='bouton retour' class='btn'></a>
+        <div class="cont">
+            <form action="" method="get">
+                <h2>Qui est notre nouvel ami ?</h2>
 
-    <div class="cont-form">
-        <form action="" method="get">
-            <h2>Qui est notre nouvel ami ?</h2>
+                <div class="hbox">
+                    <div class="vbox">
+                        <div>
+                            <label for="nom">Son nom : </label>
+                            <input type="text" name="nom">
+                        </div>
 
-            <div>
-                <div>
-                    <label for="nom">Son nom : </label>
-                    <input type="text" name="nom">
+                        <div>
+                            <label for="espece">Son espece :</label>
+                            <input type="text" name="espece">
+                        </div>
+
+                        <div>
+                            <label for="genre">Son genre : </label>
+                            <select name="genre">
+                                <option value="0">male</option>
+                                <option value="1">femele</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="vbox">
+                        <div>
+                            <label for="race">Sa race :</label>
+                            <input type="text" name="race">
+                        </div>
+        
+                        <div>
+                            <label for="taille">Sa taille : </label>
+                            <input type="number" name="taille">
+                        </div>
+
+                        <div>
+                            <label for="poids">Son poids : </label>
+                            <input type="number" name="poids">
+                        </div>
+                    </div>
+                </div>
+                <div class="vbox">
+                    <div>
+                        <label for="cast">Castration : </label>
+                        <select name="cast">
+                            <option value="0">pas castrer</option>
+                            <option value="1">castrer</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <label for="vac">Vaccins : </label>
+                        <input type="text" name="vac">
+                    </div>
+
+                    <input type="submit" value="Valider">
                 </div>
 
-                <div>
-                    <label for="espece">Son espece :</label>
-                    <input type="text" name="espece">
-                </div>
+                <?php 
+                    if (isset($_GET['nom']) && isset($_GET['espece']) && isset($_GET['race']) && isset($_GET['taille']) && isset($_GET['poids']) && isset($_GET['genre']) && isset($_GET['cast']) && isset($_GET['vac'])) {
+                    
+                        $nom = $_GET['nom'];
+                        $espece = $_GET['espece'];
+                        $race = $_GET['race'];
+                        $taille = $_GET['taille'];
+                        $poids = $_GET['poids'];
+                        $genre = $_GET['genre'];
+                        $cast = $_GET['cast'];
+                        $vac = $_GET['vac'];
 
-                <div>
-                    <label for="race">Sa race :</label>
-                    <input type="text" name="race">
-                </div>
+                        $req = "INSERT INTO projet.animaux 
+                                (nom, race, taille, genre, vaccinations, poids, numprop, espece, castration) 
+                                VALUES
+                                ('$nom', '$race', $taille, $genre, '$vac', $poids, $numprop, '$espece', $cast)";
 
-                <div>
-                    <label for="taille">Sa taille : </label>
-                    <input type="number" name="taille">
-                </div>
-
-                <div>
-                    <label for="poids">Son poids : </label>
-                    <input type="number" name="poids">
-                </div>
-
-                <div>
-                    <label for="genre">Son genre : </label>
-                    <select name="genre">
-                        <option value="0">male</option>
-                        <option value="1">femele</option>
-                    </select>
-                </div>
-
-                <div>
-                    <label for="cast">Castration : </label>
-                    <select name="cast">
-                        <option value="0">pas castrer</option>
-                        <option value="1">castrer</option>
-                    </select>
-                </div>
-
-                <div>
-                    <label for="vac">Vaccins : </label>
-                    <input type="text" name="vac">
-                </div>
-            </div>
-
-            <input type="submit" value="Valider">
-
-            <?php 
-                if (isset($_GET['nom']) && isset($_GET['espece']) && isset($_GET['race']) && isset($_GET['taille']) && isset($_GET['poids']) && isset($_GET['genre']) && isset($_GET['cast']) && isset($_GET['vac'])) {
-                
-                    $nom = $_GET['nom'];
-                    $espece = $_GET['espece'];
-                    $race = $_GET['race'];
-                    $taille = $_GET['taille'];
-                    $poids = $_GET['poids'];
-                    $genre = $_GET['genre'];
-                    $cast = $_GET['cast'];
-                    $vac = $_GET['vac'];
-
-                    $req = "INSERT INTO projet.animaux 
-                            (nom, race, taille, genre, vaccinations, poids, numprop, espece, castration) 
-                            VALUES
-                            ('$nom', '$race', $taille, $genre, '$vac', $poids, $numprop, '$espece', $cast)";
-
-                    if (!($cnx->query($req))) {
-                        echo $req;
-                    } else {
-                        header("location : profil.php");
-                    }
-                }         
-            ?> 
-        </form>
+                        if (!($cnx->query($req))) {
+                            echo $req;
+                        } else {
+                            header("location : profil.php");
+                        }
+                    }         
+                ?>
+            </form>
+        </div>
     </div>
-    
 </body>
 </html>
